9fb774acbb63fa7b05be02d076c65d166222c1ce
BirikyC <99211723+BirikyC@users.noreply.github.com>
2026-01-10 11:34:53 +0100

    Player View

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,19 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector2 input;
+    
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float rotationSpeed = 10.0f;
+    private PlayerRotation rotation;
+    private float currentAngle;
+
+    public enum PlayerRotation
+    {
+        Up, UpRight,
+        Right, RightDown,
+        Down, DownLeft,
+        Left, LeftUp
+    }
 
     void Start()
     {
@@ -15,10 +27,58 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         rb.linearVelocity = input * speed;
+
+        float targetAngle = GetRotationAngle();
+
+        currentAngle = Mathf.LerpAngle(
+            currentAngle,
+            targetAngle,
+            rotationSpeed * Time.fixedDeltaTime
+        );
+
+        rb.MoveRotation(currentAngle);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         input = context.ReadValue<Vector2>();
+
+        bool isUp = input.y > 0.5f;
+        bool isRight = input.x > 0.5f;
+        bool isDown = input.y < -0.5f;
+        bool isLeft = input.x < -0.5f;
+
+        if (isUp && isRight)
+            rotation = PlayerRotation.UpRight;
+        else if (isRight && isDown)
+            rotation = PlayerRotation.RightDown;
+        else if (isDown && isLeft)
+            rotation = PlayerRotation.DownLeft;
+        else if (isLeft && isUp)
+            rotation = PlayerRotation.LeftUp;
+        else if (isUp)
+            rotation = PlayerRotation.Up;
+        else if (isRight)
+            rotation = PlayerRotation.Right;
+        else if (isDown)
+            rotation = PlayerRotation.Down;
+        else if (isLeft)
+            rotation = PlayerRotation.Left;
+    }
+
+    private float GetRotationAngle()
+    {
+        return rotation switch
+        {
+            PlayerRotation.Right => 0f,
+            PlayerRotation.UpRight => 45f,
+            PlayerRotation.Up => 90f,
+            PlayerRotation.LeftUp => 135f,
+            PlayerRotation.Left => 180f,
+            PlayerRotation.DownLeft => 225f,
+            PlayerRotation.Down => 270f,
+            PlayerRotation.RightDown => 315f,
+            _ => 0f
+        };
     }
 }
