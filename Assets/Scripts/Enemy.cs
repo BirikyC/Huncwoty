868d8c7b3f6bede1671b8bfb107dde9cb4ec49d5
mi2no <michu.nogalski@gmail.com>
2026-01-10 18:53:44 +0100

    A*

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour
 {
@@ -8,7 +9,10 @@ public class Enemy : MonoBehaviour
 
     float speed = 1.0f;
 
-    static Tilemap tilemap;
+    public /*static*/ Tilemap tilemap;
+
+    List<Vector2Int> tiles = null;
+    int tile_id = 0;
 
     [SerializeField] private NoiseManager noiseManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -27,8 +31,18 @@ public class Enemy : MonoBehaviour
     {
         if (chase) 
         {
-            Vector2 dir = (focus_pos - (Vector2)transform.position).normalized;
+            //Vector2 dir = (focus_pos - (Vector2)transform.position).normalized;
+            //transform.position = (Vector2)transform.position + dir * speed * Time.deltaTime;
+
+            Vector2 worldPos = tilemap.CellToWorld(new Vector3Int(tiles[tile_id].x, tiles[tile_id].y, 0));
+            Vector2 dir = (worldPos - (Vector2)transform.position).normalized;
             transform.position = (Vector2)transform.position + dir * speed * Time.deltaTime;
+
+            if ((Vector2)transform.position == worldPos)
+            {
+                --tile_id;
+                chase = tile_id > 0;// tiles.Count;
+            }
         }
     }
 
@@ -36,5 +50,19 @@ public class Enemy : MonoBehaviour
     {
         focus_pos = pos;
         chase = true;
+
+        Physics2D.Raycast(transform.position, (focus_pos - (Vector2)transform.position).normalized);
+
+        tiles = PathFInd.FindPath((Vector2Int)tilemap.WorldToCell(transform.position), (Vector2Int)tilemap.WorldToCell(focus_pos), tilemap, gameObject);
+        if (tiles == null) chase = false; 
+        else tile_id = tiles.Count - 1;
+
+        Debug.Log(tiles == null);
+
+        if (tiles != null)
+        {
+            foreach (Vector2Int v in tiles) Debug.Log(v);
+            Debug.Log("");
+        }
     }
 }
