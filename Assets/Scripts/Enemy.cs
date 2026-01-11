bfeb6fed32683963a1ecef0b0fc30d344d638902
BirikyC <adam.ceglarz18@gmail.com>
2026-01-11 02:29:41 +0100

    A* algorithm

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,26 +1,135 @@
+//using UnityEngine;
+//using UnityEngine.Tilemaps;
+
+//public class Enemy : MonoBehaviour
+//{
+//    private Rigidbody2D rb;
+
+//    [SerializeField] private float avoidDistance = 1.0f;
+//    [SerializeField] private float obstacleCheckDistance = 0.8f;
+//    [SerializeField] private LayerMask obstacleLayer;
+
+//    [SerializeField] private float speed = 5.0f;
+//    [SerializeField] private float hearNoiseRadius = 5.0f;
+
+//    private bool isChasing = false;
+//    private bool isAvoidingObstacle = false;
+//    private Vector2 avoidDirection;
+//    private Vector3 targetPosition;
+
+//    private void Start()
+//    {
+//        rb = GetComponent<Rigidbody2D>();
+//    }
+
+//    void OnEnable()
+//    {
+//        NoiseManager.OnMadeNoise += HandleNoise;
+//    }
+
+//    private void OnDisable()
+//    {
+//        NoiseManager.OnMadeNoise -= HandleNoise;
+//    }
+
+//    private void FixedUpdate()
+//    {
+//        if (!isChasing) return;
+
+//        Vector2 toTarget = (targetPosition - transform.position).normalized;
+
+//        if (!isAvoidingObstacle)
+//        {
+//            if (IsObstacleAhead(toTarget))
+//            {
+//                StartAvoiding(toTarget);
+//            }
+//            else
+//            {
+//                rb.linearVelocity = toTarget * speed;
+//            }
+//        }
+//        else
+//        {
+//            rb.linearVelocity = avoidDirection * speed;
+
+//            if (!IsObstacleAhead(toTarget))
+//            {
+//                isAvoidingObstacle = false;
+//            }
+//        }
+
+//        if (Vector2.Distance(transform.position, targetPosition) < 0.1f)
+//        {
+//            Stop();
+//        }
+//    }
+
+
+//    private void HandleNoise(Vector2 noisePosition)
+//    {
+//        if (Vector2.Distance(transform.position, noisePosition) > hearNoiseRadius) return;
+
+//        targetPosition = noisePosition;
+//        isChasing = true;
+//    }
+
+//    private void Stop()
+//    {
+//        isChasing = false;
+//        rb.linearVelocity = Vector2.zero;
+//    }
+
+//    private bool IsObstacleAhead(Vector2 direction)
+//    {
+//        RaycastHit2D hit = Physics2D.Raycast(
+//            transform.position,
+//            direction,
+//            obstacleCheckDistance,
+//            obstacleLayer
+//        );
+
+//        return hit.collider != null;
+//    }
+
+//    private void StartAvoiding(Vector2 toTarget)
+//    {
+//        isAvoidingObstacle = true;
+
+//        Vector2 left = new Vector2(-toTarget.y, toTarget.x);
+//        Vector2 right = new Vector2(toTarget.y, -toTarget.x);
+
+//        float leftScore = Vector2.Distance(
+//            (Vector2)transform.position + left,
+//            targetPosition
+//        );
+
+//        float rightScore = Vector2.Distance(
+//            (Vector2)transform.position + right,
+//            targetPosition
+//        );
+
+//        avoidDirection = leftScore < rightScore ? left : right;
+//    }
+
+//}
+
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    private Vector2 focus_pos;
+    private bool chase = false;
 
-    [SerializeField] private float avoidDistance = 1.0f;
-    [SerializeField] private float obstacleCheckDistance = 0.8f;
-    [SerializeField] private LayerMask obstacleLayer;
-
-    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float speed = 1.0f;
     [SerializeField] private float hearNoiseRadius = 5.0f;
 
-    private bool isChasing = false;
-    private bool isAvoidingObstacle = false;
-    private Vector2 avoidDirection;
-    private Vector3 targetPosition;
+    public /*static*/ Tilemap tilemap;
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    List<Vector2> points = null;
+    int point_id = 0;
 
     void OnEnable()
     {
@@ -34,82 +143,68 @@ public class Enemy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (!isChasing) return;
-
-        Vector2 toTarget = (targetPosition - transform.position).normalized;
-
-        if (!isAvoidingObstacle)
+        if (chase)
         {
-            if (IsObstacleAhead(toTarget))
+            Vector2 dir = (points[point_id] - (Vector2)transform.position).normalized;
+            transform.position = (Vector2)transform.position + dir * speed * Time.deltaTime;
+
+            if ((points[point_id] - (Vector2)transform.position).normalized != dir) // Point overshot
             {
-                StartAvoiding(toTarget);
-            }
-            else
-            {
-                rb.linearVelocity = toTarget * speed;
+                ++point_id;
+                chase = point_id < points.Count;
             }
         }
+    }
+
+    public void HandleNoise(Vector2 pos)
+    {
+        float dist = Vector2.Distance(transform.position, pos);
+
+        if (dist > hearNoiseRadius) return;
+
+        chase = false;
+
+        focus_pos = pos;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, (focus_pos - (Vector2)transform.position).normalized, dist, PathFInd.OBSTACLE_MASK);
+
+        Gizmos.color = Color.green;
+
+        if (!hit)
+        {
+            points = new List<Vector2> { pos };
+            Debug.Log(PathFInd.OBSTACLE_MASK);
+            chase = true;
+            point_id = 0;
+            return;
+        }
+
+        points = PathFInd.FindPath(transform.position, focus_pos, tilemap);
+
+        if (points == null) chase = false;
         else
         {
-            rb.linearVelocity = avoidDirection * speed;
-
-            if (!IsObstacleAhead(toTarget))
-            {
-                isAvoidingObstacle = false;
-            }
+            point_id = 0;
+            chase = true;
         }
 
-        if (Vector2.Distance(transform.position, targetPosition) < 0.1f)
+    }
+
+    void OnDrawGizmos()
+    {
+        if (points == null) return;
+
+        Gizmos.color = Color.green;
+
+        if (points.Count == 1)
         {
-            Stop();
+            Gizmos.DrawLine(transform.position, points[0]);
+            return;
+        }
+
+        foreach (Vector2 c in points)
+        {
+            Gizmos.DrawWireCube(c, Vector3.one * 0.3f);
         }
     }
-
-
-    private void HandleNoise(Vector2 noisePosition)
-    {
-        if (Vector2.Distance(transform.position, noisePosition) > hearNoiseRadius) return;
-
-        targetPosition = noisePosition;
-        isChasing = true;
-    }
-
-    private void Stop()
-    {
-        isChasing = false;
-        rb.linearVelocity = Vector2.zero;
-    }
-
-    private bool IsObstacleAhead(Vector2 direction)
-    {
-        RaycastHit2D hit = Physics2D.Raycast(
-            transform.position,
-            direction,
-            obstacleCheckDistance,
-            obstacleLayer
-        );
-
-        return hit.collider != null;
-    }
-
-    private void StartAvoiding(Vector2 toTarget)
-    {
-        isAvoidingObstacle = true;
-
-        Vector2 left = new Vector2(-toTarget.y, toTarget.x);
-        Vector2 right = new Vector2(toTarget.y, -toTarget.x);
-
-        float leftScore = Vector2.Distance(
-            (Vector2)transform.position + left,
-            targetPosition
-        );
-
-        float rightScore = Vector2.Distance(
-            (Vector2)transform.position + right,
-            targetPosition
-        );
-
-        avoidDirection = leftScore < rightScore ? left : right;
-    }
-
 }
---
+++ Assets/Scripts/PathFInd.cs
@@ -0,0 +1,154 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+
+public class PathFInd : MonoBehaviour
+{
+
+    static HashSet<Vector2Int> blockedTiles = new HashSet<Vector2Int>();
+
+    public Tilemap obstacles;
+
+    public static LayerMask OBSTACLE_MASK = 0;
+
+    private void Awake()
+    {
+        BoundsInt bounds = obstacles.cellBounds;
+
+        for (int x = bounds.xMin; x < bounds.xMax; x++)
+        {
+            for (int y = bounds.yMin; y < bounds.yMax; y++)
+            {
+                Vector3Int cell = new Vector3Int(x, y, 0);
+                if (obstacles.HasTile(cell))
+                {
+                    blockedTiles.Add((Vector2Int)cell);
+                    Debug.Log(cell);
+                }
+            }
+        }
+
+        if (OBSTACLE_MASK == 0) OBSTACLE_MASK = 1 << LayerMask.NameToLayer("Obstacle");
+    }
+
+    class Node
+    {
+        public Vector2Int pos;
+        public float hCost, gCost;
+        public Node parent;
+
+        public float fCost => hCost + gCost;
+
+        public Node(Vector2Int pos, float hCost, float gCost, Node parent) 
+        { 
+            this.pos = pos;
+            this.hCost = hCost;
+            this.gCost = gCost;
+            this.parent = parent;
+        }
+    }
+
+    public static List<Vector2> FindPath(Vector2 from_p, Vector2 to_p, Tilemap t)
+    {
+        Vector2Int from = (Vector2Int)t.WorldToCell(from_p), to = (Vector2Int)t.WorldToCell(to_p);
+
+        Debug.Log(from + " to " + to);
+        List<Node> open = new List<Node> { new Node(from, MannDist(from, to), 0, null) };
+        List<Vector2Int> closed = new();
+
+        Node[] buff;// = new Node[4];
+
+        int max_dist = 10;
+
+        BoundsInt bounds = t.cellBounds;
+
+        while (open.Count > 0) 
+        {
+            int min_id = MinF(open);
+            Node q = open[min_id];
+
+            if (q.pos == to) return ReconstructPath(q, t);
+
+            open.RemoveAt(min_id);
+
+            buff = Successors(q, to);
+
+            foreach (Node s in buff)
+            {
+                if (!bounds.Contains((Vector3Int)s.pos))
+                    continue;
+                if (IsBlocked(s.pos, t))
+                    continue;
+                if (closed.Contains(s.pos))
+                    continue;
+
+                int id = open.FindIndex(n => n.pos == s.pos);
+                if (id == -1)
+                    open.Add(s);
+                else if (open[id].gCost > s.gCost)
+                    open[id] = s;
+            }
+
+            closed.Add(q.pos);
+        }
+
+        return null;
+    }
+
+    static List<Vector2> ReconstructPath(Node goal, Tilemap t)
+    {
+        List<Vector2> path = new();
+        Node current = goal;
+
+        while (current.parent != null)
+        {
+            path.Add(t.CellToWorld((Vector3Int)current.pos));
+            current = current.parent;
+        }
+
+        path.Reverse();
+        return path;
+    }
+
+    static int MinF(List<Node> l) 
+    {
+        int min = 0;
+        float min_f = l[0].fCost;
+
+        for (int i = 1; i < l.Count; ++i)
+            if (l[i].fCost < min_f) 
+            {
+                min = i;
+                min_f = l[i].fCost;
+            }
+        return min;
+    }
+
+    static Node[] Successors(Node a, Vector2Int to) 
+    {
+        Node[] buff = new Node[4];
+        buff[0] = new Node(new Vector2Int(a.pos.x - 1, a.pos.y), 0, a.gCost + 1, a);
+        buff[1] = new Node(new Vector2Int(a.pos.x + 1, a.pos.y), 0, a.gCost + 1, a);
+        buff[2] = new Node(new Vector2Int(a.pos.x, a.pos.y - 1), 0, a.gCost + 1, a);
+        buff[3] = new Node(new Vector2Int(a.pos.x, a.pos.y + 1), 0, a.gCost + 1, a);
+        
+        buff[0].hCost = MannDist(buff[0].pos, to);
+        buff[1].hCost = MannDist(buff[1].pos, to);
+        buff[2].hCost = MannDist(buff[2].pos, to);
+        buff[3].hCost = MannDist(buff[3].pos, to);
+
+        return buff;
+    }
+
+    static float MannDist(Vector2Int a, Vector2Int b) 
+    {
+        return Mathf.Abs(a.x - b.x) + Mathf.Abs(a.y - b.y);
+    }
+
+    static bool IsBlocked(Vector2Int cell, Tilemap tilemap)
+    {
+        if (blockedTiles.Contains(cell)) Debug.Log("ob");
+        return blockedTiles.Contains(cell);
+    }
+
+}
